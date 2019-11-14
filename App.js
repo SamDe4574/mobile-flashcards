679c3826f15ff78c2615a3f54191ef7a27191593
H_H-XD <hussamDe8@gmail.com>
2019-11-14 23:45:17 +0300

    create navigator

--- App.js
+++ App.js
@@ -2,23 +2,43 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Decks from './Components/Decks';
 import NewDeck from './Components/NewDeck';
+import { createBottomTabNavigator, createAppContainer} from 'react-navigation';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const TabNavigator = createMaterialBottomTabNavigator({
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      Decks: { screen: Decks,
+          navigationOptions:{
+              tabBarLabel:'Decks',
+              tabBarIcon: ({ tintColor }) => (
+                  <View>
+                      <Icon style={[{color: tintColor}]} size={25} name={'list'}/>
+                  </View>),
+          }
+      },
+      NewDeck: { screen: NewDeck,
+          navigationOptions:{
+              tabBarLabel:'NewDeck',
+              tabBarIcon: ({ tintColor }) => (
+                  <View>
+                      <Icon style={[{color: tintColor}]} size={25} name={'playlist-add'}/>
+                  </View>),
+              activeColor: '#edf4f6',
+              inactiveColor: '#224b65',
+              barStyle: { backgroundColor: '#3b8dad' },
+          }
+      },
   },
-});
+  {
+    initialRouteName: "Decks",
+    activeColor: '#edf0f6',
+    inactiveColor: '#223765',
+    barStyle: { backgroundColor: '#3b69ad' },
+  },
+);
+
+
+export default createAppContainer(TabNavigator);
